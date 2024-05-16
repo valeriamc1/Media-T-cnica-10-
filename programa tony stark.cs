bf533460b59b9a3c33d1d8afc667961040f76165
valeria <cordobav102@gmail.com>
2024-05-16 14:53:51 -0500

    Creación código tony stark
    Crear archivo programa y solución necesidad tony stark

---
+++ programa tony stark.cs
@@ -0,0 +1,16 @@
+     /******************************************************************************
+      Autor: Valeria Mosquera
+     Fecha: 2024-05-16
+     Descripción: tony stark esta desarrollando un sistema de seguridad para su 
+     mansion utilizando c#.Completa el espacio en blanco para limpiar la
+     pantalla antes de mostrar el mensaje de "Acceso denegado"
+     
+     
+     *******************************************************************************/
+     using System; 
+     class program {
+      static void Main() {
+     Console.Clear();
+     Console.WriteLine("Acceso denegado");
+      }
+     }
