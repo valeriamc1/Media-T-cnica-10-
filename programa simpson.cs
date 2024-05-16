020a364c8f07416662ed4e49922bb446a7f3d47a
valeria <cordobav102@gmail.com>
2024-05-16 15:39:35 -0500

    creación de código de los simpson
    crear archivo programa para organizar un torneo de videojuegos de los simpson

---
+++ programa simpson.cs
@@ -0,0 +1,21 @@
+     /******************************************************************************
+      Autor: Valeria Mosquera y Sara alvarez
+     Fecha: 2024-05-16
+     Descripción:Los simpson estan organizndo un torneo de videojuegos y
+     necesitan un programa en c# para mostrar los nombres de los participantes.
+     Completa el espacio en blanco para imprimir los nombres en la consola
+     
+     
+     *******************************************************************************/
+     using System; 
+     class program {
+      static void Main() {
+     
+     string participante1= " Bart,";
+     string participante2= " Lisa,";
+     string participante3= " Milhouse,";
+     string participante4= " Nelson";
+     Console.WriteLine ("Nombre de los participantes son: "+ participante1 + participante2 + participante3
+     + participante4);
+      }
+     }
