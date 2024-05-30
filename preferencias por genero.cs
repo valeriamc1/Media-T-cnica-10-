bd819c5c524ec20251469c746256851610e3cd92
valeria <cordobav102@gmail.com>
2024-05-29 19:11:25 -0500

    Update preferencias por genero.cs

--- preferencias por genero.cs
+++ preferencias por genero.cs
@@ -17,23 +17,33 @@
      using System;
      class HelloWorld {
      static void Main(string[]args) {
-      // 1 Variables 
-      Console.WriteLine("Escribir 'hombre' o 'mujer' ");
-      string genero = Console.ReadLine()
-      Console.WriteLine("Escribir tu edad ");
-      int edad=  Int32.Parse(Console.ReadLine())
-     
-     // 2 mostrar menu de opciones 
-     Console.WriteLine("seleccione su genero: ");
-     Console.WriteLine("1.hombre");
-     Console.WriteLine("2. mujer");
-     
-     //validacion
-     if ( genero == "hombre"){
-         if(edad<17)
-         Console.WriteLine("sus preferencias son:Figuras de acción, Caricaturas, Series TV");
-         
-     }
-     
-     }
-     }
+      // 1. Solicitar datos del usuario
+        Console.WriteLine("Escribir 'hombre' o 'mujer': ");
+        string genero = Console.ReadLine(); 
+        Console.WriteLine("Escribir tu edad: ");
+        int edad = Int32.Parse(Console.ReadLine());
+
+        // 2. Determinar y mostrar las preferencias según el género y la edad
+        if (genero == "hombre"){
+        
+            if (edad < 18){
+            
+                Console.WriteLine("Sus preferencias son: Figuras de acción, Caricaturas, Series TV");
+            }
+            else{
+                Console.WriteLine("Sus preferencias son: Cine, Picnic, Filantropía");
+            }
+        }
+         if (genero == "mujer"){
+        
+            if (edad < 18){
+            
+                Console.WriteLine("Sus preferencias son: Muñecas, Maquillaje, Series TV");
+            }
+            else{
+            
+                Console.WriteLine("Sus preferencias son: Maquillaje, Series TV, Cine, Filantropía");
+            }
+}
+}
+}
